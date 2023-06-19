2b6ff2eea5c86edfad8697f5d8efe0482cddc9cc
Mike <marquezini.mike@outlook.com>
2023-06-19 11:00:58 -0300

    Inclusao de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
+import React from 'react'
+import Home from '../src/Pages/home/Home'
+import About from '../src/Pages/about/About'
+import Profile from '../src/Pages/profile/Profile'
+import Navbar from './components/statics/navbar/Navbar'
+import Footer from './components/statics/footer/Footer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './Pages/login/Login'
+
+
 
 import './App.css'
 
 function App() {
-  
-
-  return (
-    <>
+   return (
+    <BrowserRouter>
+      <Navbar />
+      <div>
+        <Routes>
+          <Route  path="/" element ={<Login />} />
+          <Route path='/login' element ={<Login />} />
+          <Route path='/profile' element ={<Profile />} />
+          <Route  path="/home" element ={<Home />} />
+          <Route path='/about' element ={<About />} />
+        </Routes>
+        </div>
+      <Footer />
       
-    </>
+    </BrowserRouter>
+
   )
 }
 
