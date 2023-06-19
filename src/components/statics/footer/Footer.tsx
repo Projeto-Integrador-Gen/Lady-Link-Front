bcbd33116d70cb50509d370f68076436462f9d29
Mike <marquezini.mike@outlook.com>
2023-06-19 16:53:20 -0300

    Atualizacao background e Icon Github

--- src/components/statics/footer/Footer.tsx
+++ src/components/statics/footer/Footer.tsx
@@ -1,40 +1,29 @@
 import React from 'react';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import Github from '@material-ui/icons/LinkedIn';
 import {Typography,Grid } from '@material-ui/core';
 import {Box} from '@mui/material';
+import './Footer.css'
 
 function Footer() {
     return (
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
+                    <Box className='fundoFooter1'>
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
                             <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
                         </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://www.facebook.com/generationbrasil" target="_blank" rel="noopener noreferrer">
-                                <FacebookIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                            <a href="https://www.instagram.com/generationbrasil/" target="_blank" rel="noopener noreferrer">
-                                <InstagramIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank" rel="noopener noreferrer">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
+                        <Box className="redes" display="flex" alignItems="center" justifyContent="center">                            
+                            <a href="https://github.com/Projeto-Integrador-Gen" target="_blank" rel="noopener noreferrer">
+                            <img width="48" height="48" src="https://img.icons8.com/stickers/48/github.png" alt="github"/>
                             </a>
                         </Box>
                     </Box>
-                    <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
+                    <Box className='fundoFooter2'>
                         <Box paddingTop={1}>
                             <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2023 Copyright:</Typography>
                         </Box>
-                        <Box>
-                            <a target="_blank" href="https://brasil.generation.org" rel="noopener noreferrer">
-                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">brasil.generation.org</Typography>
-                            </a>
-                        </Box>
+                       
                     </Box>
                 </Grid>
             </Grid>
