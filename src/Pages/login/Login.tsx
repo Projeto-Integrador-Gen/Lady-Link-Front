04cba659aa1f16192cbe56a60ec4896f7d7027a8
Mike <marquezini.mike@outlook.com>
2023-06-19 16:02:43 -0300

    Implementacao Login

--- src/Pages/login/Login.tsx
+++ src/Pages/login/Login.tsx
@@ -1,10 +1,36 @@
 import React from "react";
 import './Login.css'
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 
-function Login (){
+
+function Login() {
+
     return (
-        <h1> Teste - Login</h1>
-        )
-        
+        <div className="main">  	
+		<input type="checkbox" id="chk" aria-hidden="true" />
+        	<div className="signup">
+				<form>
+					<label htmlFor="chk"aria-hidden="true"> Criar Conta</label>
+					<input type="text" name="nome" placeholder="Nome Completo" />
+                    <input type="email" name="email" placeholder="Email" />
+					<input type="senha" name="pswd" placeholder="Senha" />
+					<button>Criar Conta</button>
+				</form>
+			</div>
+
+			<div className="login">
+				<form>
+					<label htmlFor="chk" aria-hidden="true"> Entrar</label>
+					<input type="email" name="email" placeholder="Email" />
+					<input type="senha" name="pswd" placeholder="Senha" />
+                    <button>Entrar</button>
+                    <FormControlLabel className="signup"control={<Checkbox value="remember" color="secondary" />}
+                     label="Lembrar-me" />
+				</form>
+			</div>
+	</div>
+    )
+
 }
 export default Login;
