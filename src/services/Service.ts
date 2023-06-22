943ced6ec713b5d5ea6c01e80432c3cc92f342d5
Mike <marquezini.mike@outlook.com>
2023-06-22 08:21:50 -0300

    Implementacao Componente Services

---
+++ src/services/Service.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export const api = axios.create({
+    baseURL: 'https://lady-link.onrender.com/'
+})
+
+
+export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
+    const resposta = await api.post(url,dados)
+    setDado(resposta.data)
+}
+
+export const login = async(url: any,dados: any,setDado: any) => { 
+    const resposta = await api.post(url,dados)
+    setDado(resposta.data.token)
+}
