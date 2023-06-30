eea1815fd01ff2a31898b2f4968ab4b6c7958186
lukas-95 <lucrecmen@gmail.com>
2023-06-30 15:54:47 -0300

    estilização navbar 2

--- src/components/statics/navbar/Navbar.tsx
+++ src/components/statics/navbar/Navbar.tsx
@@ -5,20 +5,18 @@ import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 import { createStyles, alpha, Theme, makeStyles } from '@material-ui/core/styles';
-import { Link, useNavigate } from 'react-router-dom';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
-import AddIcon from '@material-ui/icons/Add';
-import { Box } from '@material-ui/core';
+import { Box, FormControlLabel, FormGroup, Switch } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 import './Navbar.css'
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import HomeIcon from '@material-ui/icons/Home';
-import imagem from '../../../assets/Imagem/unsplash_OhKElOkQ3RE.svg'
-import { DomainDisabled, Info, InfoOutlined } from '@material-ui/icons';
-import useLocalStorage from 'react-use-localstorage';
-import { toast } from 'react-toastify';
-import { addToken } from '../../../store/tokens/actions';
-import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import {useDispatch, useSelector} from "react-redux";
+import {TokenState} from "../../../store/tokens/tokensReducer.ts";
+import { InfoOutlined } from '@material-ui/icons';
+import {addToken} from "../../../store/tokens/actions.ts";
+
+
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -36,6 +34,7 @@ const useStyles = makeStyles((theme: Theme) =>
       },
     },
     search: {
+      display: 'flex',
       position: 'relative',
       borderRadius: theme.shape.borderRadius,
       backgroundColor: alpha(theme.palette.common.white, 0.15),
@@ -77,113 +76,140 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-function Navbar() {
-  const classes = useStyles, [token, setToken] = useLocalStorage('token');
+export default function Navbar() {
+
   let navigate = useNavigate();
+  const token = useSelector<TokenState, TokenState["tokens"]>(
+      (state) => state.tokens
+  );
   const dispatch = useDispatch();
-  
-  function goLogout(){
-    dispatch(addToken(''));
-      toast.info('Usuário deslogado', {
-        position: "top-right",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: false,
-        theme: "colored",
-        progress: undefined   
-       });
-     navigate('/login')
+  function goLogout() {
+    dispatch(addToken(''))
+    alert("Tchau, até logo mais")
+    navigate('/login')
+    
+  }
+
+  const classes = useStyles(); 
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAuth(event.target.checked);
   };
 
-    
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar className='fundonavbar'>
-          <Box display="flex" justifyContent="start">
-            <Link to="/home" className="text-decorator-none">
-              <Box mx={3} className="cursor">
-              <HomeIcon />  
-              </Box>
-            </Link>
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-            <Link to="" className="text-decorator-none">
-              <Box mx={3} className="cursor">
-              <AddIcon />  
-              </Box>
-            </Link>
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-            <Link to="/about" className="text-decorator-none">
-              <Box mx={3} className="cursor">
-              <InfoOutlined />  
-              </Box>
-            </Link>
+  var navbarComponent;
 
-            <Link to="/nowhere" className="text-decorator-none">
-              <Box mx={3} className="cursor">
-              <DomainDisabled />  
+  if (token != ''){
+
+    navbarComponent = (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+          <FormGroup >
+                <FormControlLabel
+                  control={
+                    <Switch
+                      checked={auth}
+                      onChange={handleChange}
+                      aria-label="login switch"
+                    />
+                  }
+                  label={auth ? "Sair" : "tchau!!"}
+                />
+              </FormGroup>
+
+              <Box onClick={goLogout} mx={1} className="cursor">
+                <Typography variant="h6" color="inherit">
+                  logout
+                </Typography>
               </Box>
-            </Link>
-            <Link to="/posts" className="text-decorator-none">
-                            <Box mx={1} className='cursor'>
-                                <Typography variant="h6" color="inherit">
-                                    postagens
-                                </Typography>
-                            </Box>
-                        </Link>
-                        <Link to="/temas" className="text-decorator-none">
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                temas
-                            </Typography>
-                        </Box>
-                        </Link>
-                        <Link to="/formularioTema" className="text-decorator-none">
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                cadastrar tema
-                            </Typography>
-                        </Box>
-                        </Link>
-                      
-                            <Box mx={1} className='cursor' onClick={goLogout}>
-                                <Typography variant="h6" color="inherit">
-                                    logout
-                                </Typography>
-                            </Box>
-                        
+
+
+              
+
+            <Box display="flex" justifyContent="start">
+              <Link to="/home" className="text-decorator-none">
+                <Box mx={1} className="cursor">
+                  <Typography variant="h6" color="inherit">
+                    home
+                  </Typography>
+                </Box>
+              </Link>
+
+              <Link to="/posts" className="text-decorator-none">
+                <Box mx={1} className="cursor">
+                  <Typography variant="h6" color="inherit">
+                    postagens
+                  </Typography>
+                </Box>
+              </Link>
+
+              <Link to="/temas" className="text-decorator-none">
+                <Box mx={1} className="cursor">
+                  <Typography variant="h6" color="inherit">
+                    temas
+                  </Typography>
+                </Box>
+              </Link>
+
+              <Link to="/formularioTema" className="text-decorator-none">
+                <Box mx={1} className="cursor">
+                  <Typography variant="h6" color="inherit">
+                    cadastrar tema
+                  </Typography>
+                </Box>
+              </Link>
+
+              
+              
+
+              <Link to="/about" className="text-decorator-none">
+                <Box mx={3} className="cursor">
+                <Typography className={classes.title} variant="h6" noWrap color="inherit">
+                    info
+                  </Typography>
+                </Box>
+              </Link>
+
+
+            </Box>
+<div className='searchPosition'>
 
             <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="Oque você quer achar?"
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
-                inputProps={{ "aria-label": "search" }}
-              />
+            <div className={classes.searchIcon}>
+              <SearchIcon />
             </div>
-          </Box>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+            
+</div>
 
-          
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 
-          <Link to="/profile" className="text-decorator-none">
-            <Box mx={1} className="cursor">
-              <img src={imagem} alt="Foto de Perfil" />
-            </Box>
-          </Link>
-          <Link to="/login" className="text-decorator-none">
-            <Box mx={1} className="cursor">
-              <ExitToAppIcon />
-            </Box>
-          </Link>
-        </Toolbar>
-      </AppBar>
-    </div>
+  return (
+      <>
+        {navbarComponent}
+      </>
   );
-} export default Navbar;
+}
