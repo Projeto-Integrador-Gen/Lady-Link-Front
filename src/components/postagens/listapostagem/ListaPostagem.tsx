02e860d8d5a2a167a0c01ee39bc200a47c1ecc9b
Lucas <lucas_dev@outlook.com.br>
2023-07-03 14:38:01 -0300

    att design postagens

--- src/components/postagens/listapostagem/ListaPostagem.tsx
+++ src/components/postagens/listapostagem/ListaPostagem.tsx
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react'
 import { Link,useNavigate,useParams } from 'react-router-dom'
 import Postagem from '../../../models/Postagem';
 import { busca } from '../../../services/Service'
-import {Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
+import {Card, CardActions, CardContent, Button, Typography, makeStyles, CardActionArea, CardMedia } from '@material-ui/core';
 import {Box} from '@mui/material';
 import './ListaPostagem.css';
 import useLocalStorage from 'react-use-localstorage';
 import { useSelector } from 'react-redux';
 import { TokenState } from '../../../store/tokens/tokensReducer';
 import { toast } from 'react-toastify';
+import { DeleteOutlineRounded, SyncRounded } from '@material-ui/icons';
 
 
 function ListaPostagem() {
@@ -49,47 +50,54 @@ function ListaPostagem() {
 
   }, [posts.length])
 
+  const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  });
+  const classes = useStyles();
+
   return (
+    
     <>
       {
         posts.map(post => (
-          <Box m={2} >
-            <Card variant="outlined">
-              <CardContent>
-                <Typography color="textSecondary" gutterBottom>
-                  Postagens
-                </Typography>
-                <Typography variant="h5" component="h2">
-                  {post.titulo}
-                </Typography>
-                <Typography variant="body2" component="p">
-                  {post.conteudo}
-                </Typography>
-                <Typography variant="body2" component="p">
-                  {post.tema?.titulo}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Box display="flex" justifyContent="center" mb={1.5}>
+          <>
+          <Card className='teste'>
+          <CardActionArea>
+        <CardMedia
+          className='titulo'
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title={post.titulo}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+          {post.tema?.titulo}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          {post.conteudo}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
 
-                  <Link to={`/formularioPostagem/${post.id}`} className="text-decorator-none" >
-                    <Box mx={1}>
-                      <Button variant="contained" className="marginLeft" size='small' color="primary" >
-                        atualizar
-                      </Button>
-                    </Box>
-                  </Link>
-                  <Link to={`/deletarPostagem/${post.id}`} className="text-decorator-none">
-                    <Box mx={1}>
-                      <Button variant="contained" size='small' color="secondary">
-                        deletar
-                      </Button>
-                    </Box>
-                  </Link>
-                </Box>
-              </CardActions>
-            </Card>
-          </Box>
+      <CardActions className='btn-right'>
+      <Link to={`/formularioPostagem/${post.id}`} className="text-decorator-none" >
+        <Button className='btn'>
+        <SyncRounded/>
+        </Button>
+      </Link>
+      <Link to={`/deletarPostagem/${post.id}`} className="text-decorator-none">
+        <Button className='btn'>
+        <DeleteOutlineRounded/>
+        </Button>
+        </Link>
+      </CardActions>
+      <Typography className='btn-left'><p className='hash'> <img width="20" height="20" src="https://img.icons8.com/ios-filled/40/hashtag.png" alt="hashtag"/> <Link to='/temas'>  {post.tema?.titulo}  </Link> </p></Typography>
+              </Card>
+              </>
         ))
       }
     </>
