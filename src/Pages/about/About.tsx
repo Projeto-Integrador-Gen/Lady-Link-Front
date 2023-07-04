29f70c0204fa1c6c0c889626c701c154b63359a3
Mike <marquezini.mike@outlook.com>
2023-07-04 12:20:18 -0300

    Estilização About

--- src/Pages/about/About.tsx
+++ src/Pages/about/About.tsx
@@ -29,54 +29,62 @@ function About() {
 
             <React.Fragment>
                 <CssBaseline />
-                <Container maxWidth="sm">
-                    <img className='imagemlogo'src={LadyLink} alt="" />
-                    <h3 className='about'>
-                    É uma empresa dedicada a realçar a potência e o poder das mulheres no mercado de trabalho. Acreditamos que todas as mulheres possuem habilidades e talentos únicos que merecem ser reconhecidos e valorizados. Por meio da nossa plataforma de rede social, buscamos criar um ambiente inclusivo e inspirador, onde as mulheres podem se conectar, compartilhar conhecimentos e alcançar o sucesso profissional.
-                            
+                <Container fixed>
+                        <h1 className='titles'>Sobre a LadyLink  </h1>
+                        <img className='imagemlogo' src={LadyLink} alt="" />
+                   
+                    <h3 className='about alinhado'>
+                        É uma empresa dedicada a realçar a potência e o poder das mulheres no mercado de trabalho. Acreditamos que todas as mulheres possuem habilidades e talentos únicos que merecem ser reconhecidos e valorizados. Por meio da nossa plataforma de rede social, buscamos criar um ambiente inclusivo e inspirador, onde as mulheres podem se conectar, compartilhar conhecimentos e alcançar o sucesso profissional.
+
                     </h3>
 
 
                     <Typography component="div" style={{ height: '10vh' }} />
                     <Grid container spacing={3}>
-                        <Grid item xs={12}>
-                            <Paper className={classes.paper}><Typography />
-                                <h1>Valores</h1>  <img src={Valores} alt="" /><p>
-                                    <h2>Empoderamento</h2> <h3>Acreditamos no poder do empoderamento feminino para transformar vidas e impulsionar mudanças positivas na sociedade.</h3>
+                        <Grid item xs={4} >
+                            <Paper elevation={6} className={classes.paper}><Typography />
+                                <h1>Valores</h1>  <img src={Valores} alt="" />
+
+                                <Box className='alinhado'> <h2>Empoderamento</h2> <h3>Acreditamos no poder do empoderamento feminino para transformar vidas e impulsionar mudanças positivas na sociedade.</h3>
 
                                     <h2>Inclusão</h2><h3>Defendemos a diversidade e a inclusão em todos os níveis. Nossa plataforma é um espaço acolhedor para mulheres de diferentes origens, etnias, orientações e habilidades.</h3>
 
                                     <h2>Colaboração</h2><h3>Valorizamos a colaboração e o compartilhamento de conhecimentos. Através da nossa rede, incentivamos as mulheres a se apoiarem mutuamente e a construírem relacionamentos significativos.</h3>
 
                                     <h2>Excelência</h2><h3>Buscamos a excelência em tudo o que fazemos, oferecendo uma plataforma de qualidade, recursos relevantes e experiências enriquecedoras para nossas usuárias.</h3>
-
-                                </p>
+                                </Box>
 
 
                             </Paper>
                         </Grid>
-                        <Grid item xs={12}>
-                            <Paper className={classes.paper}>
-                                <h1>Visão</h1>  <img src={Visao} alt="" /><p></p>
-                                <h3>Queremos construir um mundo onde todas as mulheres tenham igualdade de oportunidades e sejam reconhecidas por seus talentos.
+                        <Grid item xs={4}>
+                            <Paper elevation={6} className={classes.paper}>
+                                <h1>Visão</h1>  <img src={Visao} alt="" />
+                                <Box className='alinhado'>
+                                    <h3>Queremos construir um mundo onde todas as mulheres tenham igualdade de oportunidades e sejam reconhecidas por seus talentos.
 
-                                    <h4>Promover a igualdade de gênero, inspirar e capacitar mulheres em suas carreiras. Buscamos quebrar barreiras, superar estereótipos e desigualdades salariais, oferecendo um ambiente de respeito e equidade.</h4>
+                                        <h4>Promover a igualdade de gênero, inspirar e capacitar mulheres em suas carreiras. Buscamos quebrar barreiras, superar estereótipos e desigualdades salariais, oferecendo um ambiente de respeito e equidade.</h4>
 
 
-                                    <h4>Nossa missão é  Através da nossa plataforma de rede social, queremos criar uma comunidade forte, onde mulheres possam se conectar, compartilhar conhecimentos e se apoiar mutuamente. Estamos comprometidos em transformar o presente e o futuro, tomando medidas concretas, estabelecendo parcerias estratégicas e programas de capacitação.</h4>
-                                </h3>
+                                        <h4>Nossa missão é  Através da nossa plataforma de rede social, queremos criar uma comunidade forte, onde mulheres possam se conectar, compartilhar conhecimentos e se apoiar mutuamente. Estamos comprometidos em transformar o presente e o futuro, tomando medidas concretas, estabelecendo parcerias estratégicas e programas de capacitação.</h4>
+                                    </h3>
+                                </Box>
+
 
                             </Paper>
                         </Grid>
                         <Grid item xs>
-                            <Paper className={classes.paper}><h1>Missao</h1><img src={Missao} alt="" /><p></p><h3>
+                            <Paper elevation={6} className={classes.paper}><h1>Missao</h1><img src={Missao} alt="" /><Box className='alinhado'> <h3>
                                 Promover a igualdade de gênero e empoderar as mulheres em suas carreiras. <h4>Queremos proporcionar oportunidades de crescimento, networking e aprendizado, ajudando-as a alcançar seus objetivos e a quebrar barreiras no mundo corporativo.</h4><h4>Acreditamos que quando as mulheres são fortalecidas e encorajadas, elas podem conquistar qualquer coisa.</h4>
-                            </h3></Paper>
+                            </h3></Box>
+
+
+                            </Paper>
                         </Grid>
                     </Grid>
 
                     <Box className='fontcenter'>
-                        <h3>Conheça nossa equipe</h3>
+                        <h2>Conheça nossa equipe</h2>
                     </Box>
                     <Grid className='iconcenter'>
 
@@ -236,14 +244,14 @@ function About() {
 
             <Grid>
 
-                <h4 className='fontcenter'>
+                <h3 className='fontcenter'>
                     Na Lady Link, estamos comprometidos em capacitar mulheres e construir um futuro mais igualitário.
-                </h4>
+                </h3>
                 <Grid className=' font fontcenter'>
 
-                    <h5>
+                    <h2>
                         Junte-se a nós nessa jornada e descubra o seu potencial!
-                    </h5>
+                    </h2>
 
 
                 </Grid>
